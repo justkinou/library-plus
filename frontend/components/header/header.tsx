c7dec2577d0e3ca4d5453443b28adb3380d0bff1
justkinou <justkinou@proton.me>
2026-04-15 14:12:39 +0200

    add shadcn/ui library

---
+++ frontend/components/header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="w-full justify-between">
+      <span className="font-bold text-2xl">Library+</span>
+    </div>
+  )
+}
+
+export default Header
