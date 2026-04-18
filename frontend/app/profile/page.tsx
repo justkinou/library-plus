10335cf3617034a263ffe5d510fbc6abcedc4413
justkinou <justkinou@proton.me>
2026-04-18 21:35:02 +0200

    add auth middleware

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>PROFILE</div>
+  )
+}
+
+export default page
--- frontend/components/header/user/user.tsx
+++ frontend/components/header/user/user.tsx
@@ -5,9 +5,7 @@ import HeaderUserMenu from './user-menu';
 
 async function HeaderUser() {
   const cookieStore = await cookies();
-  const isAuthenticated = cookieStore.has('accessToken') && cookieStore.has('refreshToken');
-
-  console.dir({ isAuthenticated })
+  const isAuthenticated = cookieStore.has('refreshToken');
 
   return (
     <div className="flex gap-2 items-center cursor-pointer transition-colors hover:text-gray-400">
