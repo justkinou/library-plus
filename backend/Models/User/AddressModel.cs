5200cc47057294abd0ae6abc2d7fa0615cc5115f
justkinou <justkinou@proton.me>
2026-05-02 08:42:49 +0200

    remove redundant fields from AddressModel

--- backend/Models/User/AddressModel.cs
+++ backend/Models/User/AddressModel.cs
@@ -5,8 +5,6 @@ namespace LibraryPlus.Models.User;
 [BsonIgnoreExtraElements]
 public class AddressModel
 {
-    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-    public string Id { get; set; } = null!;
     public string? Country { get; set; } = null;
     public string? State { get; set; } = null;
     public string? City { get; set; } = null;
